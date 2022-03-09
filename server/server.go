6950b5ac011c43e32560bb0c591f4b4f55c848d2
megatop1 <beachbumrj@optonline.net>
2022-03-09 00:00:07 -0500

    server.go initial code

--- server/server.go
+++ server/server.go
@@ -1,1 +1,17 @@
+package main
 
+import(
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    fileServer := http.FileServer(http.Dir("./static")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
+    http.Handle("/", fileServer) //handle the root route 
+    http.HandleFunc("form", formHandler) //function in HTTP package to handle another route
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Printf("Starting server at port 8080\n") //Message to print when we start our go server
+    if err := http.ListenerAndServe(":8080", nil) //
+}
