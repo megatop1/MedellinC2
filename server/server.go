3314c1db7c77685ad7b4ebe069fd73bc12e949f5
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-03-09 00:25:34 -0500

    Update server.go

--- server/server.go
+++ server/server.go
@@ -10,8 +10,21 @@ func main() {
     fileServer := http.FileServer(http.Dir("./static")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
     http.Handle("/", fileServer) //handle the root route 
     http.HandleFunc("form", formHandler) //function in HTTP package to handle another route
-    http.HandleFunc("/hello", helloHandler)
+    http.HandleFunc("/listener", listenerHandler)
 
     fmt.Printf("Starting server at port 8080\n") //Message to print when we start our go server
-    if err := http.ListenerAndServe(":8080", nil) //
+    if err := http.ListenerAndServe(":8080", nil); error != nil { //ListenAndServe function creates the server. This is the heart of the server. We can have an error, or "nil" and when not nil, log the error
+		log.Fatal(err)
+	} 
+}
+
+func listenerHandler(w http.ResponseWriter, r *http.Request) { //In any API, you have a response and a request. User sends requests, and Server sends response. * is a pointer pointing to the request
+	if r.URL.Path != "/listener" { //if request URL 
+	http.Error(w, "404 not found", http.StatusNotFound)
+	return 
+	}
+	if r.Method != "GET" {//We don't want people to be able to POST to each page. 
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}	
+	fmt.Fprintf(w, "Welcome!")
 }
