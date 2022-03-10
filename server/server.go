5f79354fb493ce1fcad1f9dd31831a5766b94241
megatop1 <beachbumrj@optonline.net>
2022-03-10 00:04:28 -0500

    web server functional

--- server/server.go
+++ server/server.go
@@ -7,9 +7,9 @@ import(
 )
 
 func main() {
-    fileServer := http.FileServer(http.Dir("./static")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
+    fileServer := http.FileServer(http.Dir("../Web")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
     http.Handle("/", fileServer) //handle the root route 
-    //http.HandleFunc("/listener", listenerHandler)
+    http.HandleFunc("/listener", listenerHandler)
 
     fmt.Printf("Starting server at port 8080\n") //Message to print when we start our go server
     if err := http.ListenAndServe(":8080", nil); err != nil { //ListenAndServe function creates the server. This is the heart of the server. We can have an error, or "nil" and when not nil, log the error
