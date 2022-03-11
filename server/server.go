7fb12d1dc9e4ddf33df84f5542e8e987d114b8ac
megatop1 <beachbumrj@optonline.net>
2022-03-10 19:31:19 -0500

    Cleaned up code with webserver and TCP server

--- server/server.go
+++ server/server.go
@@ -2,22 +2,25 @@ package main
 
 import(
     "fmt"
-  //  "log"
-  //  "net/http"
+    "log"
+    "net/http"
     "net"
     "bufio"
 )
 
 func main() {
-    //fileServer := http.FileServer(http.Dir("../Web")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
-    //http.Handle("/", fileServer) //handle the root route 
-
-    //fmt.Printf("Starting server at port 8080\n") //Message to print when we start our go server
-    //if err := http.ListenAndServe(":8080", nil); err != nil { //ListenAndServe function creates the server. This is the heart of the server. We can have an error, or "nil" and when not nil, log the error
-//		log.Fatal(err)
-//	} 
-
     listener()
+    webserver()
+}
+
+func webserver() {
+    fileServer := http.FileServer(http.Dir("../Web")) //The := declares and defines a variable. We are telling go we want to look at the directory named "static". We are creating a path.
+    http.Handle("/", fileServer) //handle the root route 
+
+    fmt.Printf("Starting server at port 8080\n") //Message to print when we start our go server
+    if err := http.ListenAndServe(":8080", nil); err != nil { //ListenAndServe function creates the server. This is the heart of the server. We can have an error, or "nil" and when not nil, log the error
+		log.Fatal(err)
+	} 
 }
 
 func listener() {
