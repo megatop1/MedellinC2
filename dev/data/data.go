8e5e182d00a808f88f2d7817b4749a049adddbf6
megatop1 <beachbumrj@optonline.net>
2022-03-11 00:52:00 -0500

    sqlite first push

--- dev/data/data.go
+++ dev/data/data.go
@@ -2,8 +2,10 @@ package data
 
 import (
 	"database/sql" //go database driver package
+	"log"
 
-	_ "github.com/mattn/go-sqlite3" //sqlite3 package
+	//sqlite3 package
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //package level variable to hold our database instance
@@ -14,11 +16,26 @@ func OpenDatabase() error {
 	//error is only return value
 	var err error
 
-	db, err = sql.Open("sqlite3,", "./sqlite-database.db") //driver name and location of DB. sql.Open returns DB instance and error
+	db, err = sql.Open("sqlite3", "./sqlite-database.db") //driver name and location of DB. sql.Open returns DB instance and error
 	//if we hit an error we just return it
 	if err != nil {
 		return err
 	}
 	//return error we get when we ping the db connection
 	return db.Ping()
+}
+
+func CreateTable() {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS tests (
+		"idNote" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"word" TEXT	
+	);`
+
+	statement, err := db.Prepare(createTableSQL) //db.Prepare returns a SQL statement and an error
+	if err != nil {
+		log.Fatal(err.Error()) //log the error if we get it
+	}
+
+	statement.Exec()
+	log.Println("Users table created")
 }
