845b2fc29e1bb000bbf4dc0535b8c442535323fd
megatop1 <beachbumrj@optonline.net>
2022-03-11 23:06:20 -0500

    listener up to struct

--- dev/cmd/create.go
+++ dev/cmd/create.go
@@ -5,8 +5,11 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -29,5 +32,35 @@ type promptContent struct {
 
 func init() {
 	listenersCmd.AddCommand(createCmd)
+}
 
+func promptGetInput(pc promptContent) string {
+	validate := func(input string) error { //validate function to ensure the user enters input
+		if len(input) <= 0 {
+			return errors.New(pc.errorMsg)
+		}
+		return nil
+	}
+
+	templates := &promptui.PromptTemplates{ //template will style different parts of the prompt
+		Prompt:  "{{ . }}",
+		Valid:   "{{ . | green }}",
+		Invalid: "{{ . | red }}",
+		Success: "{{ . | bold }}",
+	}
+
+	prompt := promptui.Prompt{ //combine the templates and validate function to determine behavior of input prompt
+		Label:     pc.label,
+		Templates: templates,
+		Validate:  validate,
+	}
+
+	result, err := prompt.Run() //now that config is done we can run our prompt. Run method returns result from the user and an error
+	if err != nil {             // quick error handling
+		fmt.Printf("Prompt Failed %v\n", err) //let user know the prompt has failed
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input: %s\n", result)
+	return result
 }
